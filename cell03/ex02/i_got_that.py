d5f47bcc21c9808463cfc7bbcf92566c5ab0f6b5
mkamkaew <methineekamkaew608@gmail.com>
2025-03-11 10:11:01 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+user_input = str(input("What you gonna say? : "))
+
+while True :
+    if user_input == "stop" :
+        break
+    
+    user_input = str(input("I got that! Anything else? : "))
+
+    
