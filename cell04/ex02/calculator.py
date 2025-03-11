d850415dfa1cca52a69b7e351a9e4532c2af3d69
mkamkaew <methineekamkaew608@gmail.com>
2025-03-11 12:45:21 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+num_1_input = int(input("Give me the first number: "))
+num_2_input = int(input("Give me the second number: "))
+
+print("Thank you!!")
+
+print(f"{num_1_input} + {num_2_input} = {num_1_input + num_2_input}")
+print(f"{num_1_input} - {num_2_input} = {num_1_input - num_2_input}")
+print(f"{num_1_input} / {num_2_input} = {num_1_input / num_2_input}")
+print(f"{num_1_input} * {num_2_input} = {num_1_input * num_2_input}")
