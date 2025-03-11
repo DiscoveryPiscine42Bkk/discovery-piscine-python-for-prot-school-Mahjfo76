13447273be6da566652d5a366c81992bc9981bd6
mkamkaew <methineekamkaew608@gmail.com>
2025-03-11 10:44:00 +0700

    Add files via upload

---
+++ cell03/ex01/multiplication_table ver for loop.py
@@ -0,0 +1,13 @@
+num_input = int(input("Enter your number : "))
+x = 0
+
+while True :
+    print(x, " x ", num_input, " = ", x*num_input)
+    
+    x = x + 1
+
+    if x > 12 :
+        break
+
+#for x in range(1, 13) :
+    #print(x, " x ", num_input, " = ", x*num_input)
