9762bd698793e8713905d0e4739c78fc952afb11
mkamkaew <methineekamkaew608@gmail.com>
2025-03-13 09:00:02 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,6 +1,17 @@
 number = [2, 8, 9, 48, 8, 22, -12, 2]
 
+new_num = []
+
 print(f"Original array:{number}")
 
-number = [x + 2 for x in number]
-print(f"New array:{number}")
+x = 0 
+
+while x < 8 :
+    new_num.append(number[x] + 2)
+
+    x += 1
+
+
+
+# number = [x + 2 for x in number]
+print(f"New array:{new_num}")
