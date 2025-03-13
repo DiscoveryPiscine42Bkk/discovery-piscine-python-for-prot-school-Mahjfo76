4234043c136469da3bdb5e1eb3e04bc806d60487
mkamkaew <methineekamkaew608@gmail.com>
2025-03-13 11:54:08 +0700

    Add files via upload

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,17 @@
+#user_input = str(input())
+
+#if len(user_input) == 0 :
+#    print("None")
+
+#else :
+#    print(user_input.upper())
+
+import sys
+
+user_input = " ".join(sys.argv[1:])
+
+if len(sys.argv) == 0 :
+    print("None")
+
+else :
+    print(user_input.upper())
