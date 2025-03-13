1f8131fc8530de7d62a1dc408e4f1bed3a58c9c3
mkamkaew <methineekamkaew608@gmail.com>
2025-03-13 09:24:29 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,12 +1,14 @@
 number = [2, 8, 9, 48, 8, 22, -12, 2]
 new_number = set()
-
+num = 0
 
 print(number)
 
-for num in number :
-    if num > 5 :
-        new_number.add(num + 2)
+while num < 8 :
 
+    if number[num] > 5 :
+        new_number.add(number[num] + 2)
+
+    num += 1
 
 print(new_number)
