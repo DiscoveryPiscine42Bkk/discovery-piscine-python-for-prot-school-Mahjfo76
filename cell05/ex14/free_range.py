cc3c0f367dc8d7fef37a3c9706c8c17e391b468b
mkamkaew <methineekamkaew608@gmail.com>
2025-03-14 13:45:37 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,7 +1,7 @@
 import sys
 
 if len(sys.argv) == 3 :
-    a = [new for new in range(int(sys.argv[1]),int(sys.argv[2]) + 1)]
+    a = list(range(int(sys.argv[1]),int(sys.argv[2]) + 1))
     print(a)
 
 else :
