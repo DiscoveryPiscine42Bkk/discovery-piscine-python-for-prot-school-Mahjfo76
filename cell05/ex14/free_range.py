bde434e81bd138fffb99f954b88fe8ec4244e8d4
mkamkaew <methineekamkaew608@gmail.com>
2025-03-14 14:14:45 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,8 +1,12 @@
 import sys
 
 if len(sys.argv) == 3 :
-    a = list(range(int(sys.argv[1]),int(sys.argv[2]) + 1))
-    print(a)
+        if sys.argv[2] < sys.argv[1] :
+             a = list(range(int(sys.argv[1]),int(sys.argv[2]) - 1, -1)) 
+             print(a)
+        else :
+            a = list(range(int(sys.argv[1]),int(sys.argv[2]) + 1))
+            print(a)
 
 else :
     print("None")
